2b7a68576a0f3a3c2f21d54261b657614b8d1bf5
shuliuzhenhua <971270272@qq.com>
2020-06-20 08:37:52 +0800

    test prettier

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,26 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png" />
+		<HelloWorld msg="Welcome to Your Vue.js App" />
+	</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  },
+	name: 'Home',
+	components: {
+		HelloWorld
+	},
 	methods: {
-    test () {
-      console.log('aaa');
-      console.log('bbb');
-			console.log('cccc');
-			console.log('cccc');
-
-    }
+		test() {
+			console.log('aaa')
+			console.log('bbb')
+			console.log('cccc')
+			console.log('cccc')
+		}
 	}
-};
+}
 </script>
